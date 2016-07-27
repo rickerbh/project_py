54cceffb465068f76f117f9b2b80e2e939216705
Hamish Rickerby <hamish@simplemachines.com.au>
2016-07-27 21:11:57 +1000

    Update test structure

--- tests/example.py
+++
@@ -1,4 +0,0 @@
-from nose.tools import *
-
-def run_a_test():
-    assert(True, True)
---
+++ tests/example_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import *
+import unittest
+
+class MyTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+    def run_a_test(self):
+        assert False
+        
